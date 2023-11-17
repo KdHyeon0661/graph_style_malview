394cc6e5949088b4859869294085e7b95440d21e
DESKTOP-RHFVAAG\rlaeh <tnghwk0661@gmail.com>
2023-11-17 20:01:37 +0900

    v1.13

--- routes/printGraph.js
+++ routes/printGraph.js
@@ -9,13 +9,18 @@ router.use(express.json())
 router.use(express.urlencoded({ extended: false }));
 
 router.get("/:id", (req, res) => {
-    var dataToSend;
+    let dataToSend;
+    let text_file;
     const python = spawn('python', ['beautiful.py', req.params.id]);
     fs.readFile('./storeValue/' + req.params.id.toString().split('.', 1) + '.txt', 'utf8', (err, data) => {
         dataToSend = data;
     });
+    fs.readFile('./text_file/' + req.params.id.toString().split('.', 1) + '.txt', 'utf8', (err, data) => {
+        text_file = data;
+    });
+    
     python.on('close', (code) => {
-        res.render('printGraph.ejs', {whoAre:req.params.id.toString().split('.', 1)});
+        res.render('printGraph.ejs', {whoAre:req.params.id.toString().split('.', 1), whoAre2:dataToSend, thisname:text_file});
     });
 });
 
@@ -25,7 +30,7 @@ router.post("/:id", (req, res) => {
     const python = spawn('python', ['make_network_dummy.py', filename, parseFloat(req.body.t2_threshold).toFixed(2), parseFloat(req.body.e2_threshold).toFixed(2)]);
     python.on('close', (code) => {
         console.log(`child process close all stdio with code ${code}`);
-        res.redirect('/');
+        res.redirect('/printGraph/' + req.params.id.toString().split('.', 1) + '.html');
     });
 });
 
